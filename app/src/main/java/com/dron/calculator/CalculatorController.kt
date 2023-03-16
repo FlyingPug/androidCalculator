7935ab6b2a926b27aa00804dffa749560e635397
Dron <guyyellow47@gmail.com>
2023-03-17 00:23:37 +0700

    снова исправил ошибки, но в процессе пришлось использовать !!, что может быть плохой практикой, но никак не должно сказываться. Надеюсь в этот раз лучше

--- app/src/main/java/com/dron/calculator/CalculatorController.kt
+++ app/src/main/java/com/dron/calculator/CalculatorController.kt
@@ -1,23 +1,36 @@
 package com.dron.calculator
 
-import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import java.math.BigDecimal
 import java.math.RoundingMode
 
 class CalculatorController() {
-    var currentOperator = Operation.EMPTY
-    var prevNumber = "0"
-    var currentNumber = ""
-    var status = Status.WORKING
+    private val _outputColor = MutableLiveData<Status>()
+    val outputColor: LiveData<Status> get() = _outputColor
+
+    private val currentNumber = MutableLiveData(EMPTY)
+    val outputText: LiveData<String> get() = currentNumber
 
     companion object {
         const val MAX_NUMBER_LENGTH = 12
         const val DEFAULT_NUMBER = "0"
+        const val REAL_BASE = "0,"
+        const val COMMA = ","
+        const val ZERO = "0"
+        const val MINUS = "-"
+        const val EMPTY = ""
+        const val POINT = "."
+        const val ERROR_MESSAGE = "ERROR"
     }
+
+    var currentOperator = Operation.EMPTY
+    var prevNumber = ZERO
+    var status = Status.WORKING
 
     fun makeAction(operation: Operation) {
         when (operation) {
-            Operation.PROCENT -> procent()
+            Operation.PERCENT -> percent()
             Operation.AC -> clear()
             Operation.EQUAL  -> equal()
             Operation.REMOVE -> remove()
@@ -31,27 +44,22 @@ class CalculatorController() {
         }
     }
 
-    // функция, позволяющая контролировать вывод при необходимости
-    fun getOutput(): String {
-        return currentNumber
-    }
-
     fun addNumber(str: String) {
         if (status != Status.WORKING) clear() // если контроллер завершил работу, очищаем для ввода нового числа
 
-        if (currentNumber.length >= MAX_NUMBER_LENGTH) return // проверяем на соответствие длине поля (иначе
-        if (currentNumber == DEFAULT_NUMBER) clear() // если 0, убираем несущественный
-        currentNumber += str
+        if (currentNumber.value!!.length >= MAX_NUMBER_LENGTH) return // проверяем на соответствие длине поля (иначе
+        if (currentNumber.value == DEFAULT_NUMBER) clear() // если 0, убираем несущественный
+        currentNumber.value += str
     }
 
     // добавляет "," к текущему числу
     fun addDecimal() {
         if (status != Status.WORKING) clear()
-        if (currentNumber.isEmpty()) { // начинаем с 0,
-            currentNumber = "0,"
+        if (currentNumber.value!!.isEmpty()) { // начинаем с 0,
+            currentNumber.value = REAL_BASE
             return
         }
-        if (!currentNumber.contains(",")) currentNumber += "," // если запятой уже нет, добавляем
+        if (!currentNumber.value!!.contains(COMMA)) currentNumber.value += COMMA // если запятой уже нет, добавляем
     }
 
     // указать текущее  действие ( + - * / )
@@ -61,14 +69,14 @@ class CalculatorController() {
             clear()
             return
         }
-        if (currentNumber.isEmpty()) prevNumber = "0" // если нету числа, c которым будем выполнять операцию, будем выполнять ее с 0
+        if (currentNumber.value!!.isEmpty()) prevNumber =  ZERO // если нету числа, c которым будем выполнять операцию, будем выполнять ее с 0
         if (currentOperator != Operation.EMPTY) // если это уже не первое действие, выисляем предыдущее значение
         {
             equal()
         }
-        prevNumber = currentNumber.toString() // сохраняем текущее число
+        prevNumber = currentNumber.value.toString() // сохраняем текущее число
         currentOperator = newOperator // сохраняем текущее действие
-        currentNumber = "" // подготавливаем для работы с новым числом
+        currentNumber.value = "" // подготавливаем для работы с новым числом
     }
 
     // меняет знак с между + и -
@@ -77,10 +85,11 @@ class CalculatorController() {
             Status.WORKING // продолжаем действие с вычесленным числом
         else if (status == Status.ERROR) {
             clear()
+            _outputColor.value = status
             return
         }
-        if (currentNumber.startsWith("-")) currentNumber = currentNumber.replace("-", "");
-        else currentNumber = "-${currentNumber}"
+        if (currentNumber.value!!.startsWith(MINUS)) currentNumber.value = currentNumber.value!!.replace(MINUS, EMPTY)
+        else currentNumber.value = MINUS + currentNumber.value
     }
 
     // вычисляет значение выражения
@@ -89,10 +98,9 @@ class CalculatorController() {
             return
         }
         try {
-            val currentOperand = currentNumber.replace(",", ".").toDouble()
-            val secondOperand = prevNumber.replace(",", ".").toDouble()
-            var result = 0.0
-            result = when (currentOperator) {
+            val currentOperand = currentNumber.value!!.replace(COMMA, POINT).toDouble()
+            val secondOperand = prevNumber.replace(COMMA, POINT).toDouble()
+            val result = when (currentOperator) {
                 Operation.PLUS -> currentOperand + secondOperand
                 Operation.MINUS -> secondOperand - currentOperand
                 Operation.MULTIPLE -> currentOperand * secondOperand
@@ -103,36 +111,37 @@ class CalculatorController() {
             }
             clear()
             // благодоря bigdecimal избавляемся от незначимых нулей
-            currentNumber =
+            currentNumber.value =
                 BigDecimal(result).setScale(5, RoundingMode.HALF_UP).stripTrailingZeros()
-                    .toPlainString().replace(".", ",")
+                    .toPlainString().replace(POINT, COMMA)
             status =
                 Status.DONE// говорим, что результат выражения выведен и мы готовы к работе с новым выражением
         } catch (e: Exception) {
-            currentNumber = "ERROR"
+            currentNumber.value = ERROR_MESSAGE
             status = Status.ERROR
         }
     }
 
     // очистка экрана
     fun clear() {
-        currentNumber = ""
+        currentNumber.value = EMPTY
         currentOperator = Operation.EMPTY
         prevNumber = DEFAULT_NUMBER
         status = Status.WORKING
+        _outputColor.value = status
     }
 
     // тупо деление числа на 100
-    fun procent() {
+    fun percent() {
         if (status == Status.DONE) status =
             Status.WORKING // продолжаем действие с вычесленным числом
         else if (status == Status.ERROR) {
             clear()
             return
         }
-        if (currentNumber.isEmpty()) return
-        val number = currentNumber.replace(",", ".").toDouble()
-        currentNumber = (number / 100).toString().replace(".", ",")
+        if (currentNumber.value!!.isEmpty()) return
+        val number = currentNumber.value!!.replace(COMMA, POINT).toDouble()
+        currentNumber.value = (number / 100).toString().replace(POINT, COMMA)
     }
 
     // удаление последнего символа
@@ -143,8 +152,8 @@ class CalculatorController() {
             clear()
             return
         }
-        if (currentNumber.isEmpty()) return
-        currentNumber = currentNumber.dropLast(1)
+        if (currentNumber.value!!.isEmpty()) return
+        currentNumber.value = currentNumber.value!!.dropLast(1)
 
     }
 
--- app/src/main/java/com/dron/calculator/MainActivity.kt
+++ app/src/main/java/com/dron/calculator/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import android.view.animation.AnimationUtils.loadAnimation
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
 import com.dron.calculator.databinding.ActivityMainBinding
 
 
@@ -18,14 +15,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val animationAlpha = loadAnimation(this, R.anim.alpha);
+        val animationAlpha = loadAnimation(this, R.anim.alpha)
         val calculator = CalculatorController()
 
         val normalColor = ContextCompat.getColor(this, R.color.numberColor)
         val errorColor = ContextCompat.getColor(this, R.color.errorColor)
 
         // возможно это не самый красивый способ, и имеет смысл пройтись по группе кнопок циклом, но хотяб удобно
-        var numberButtons = listOf(
+        val numberButtons = listOf(
             binding.button0,
             binding.button1,
             binding.button2,
@@ -42,8 +39,6 @@ class MainActivity : AppCompatActivity() {
             numberButton.setOnClickListener {
                 calculator.addNumber(index.toString())
                 it.startAnimation(animationAlpha)
-                binding.outputText.text = calculator.getOutput()
-                changeColorBasedOnStatus(calculator.status, normalColor, errorColor)
             }
         }
 
@@ -51,7 +46,7 @@ class MainActivity : AppCompatActivity() {
             binding.buttonDecimal to Operation.DECIMAL,
             binding.buttonAC to Operation.AC,
             binding.buttonPlusMinus to Operation.CHANGE_SIGN,
-            binding.buttonProcent to Operation.PROCENT,
+            binding.buttonProcent to Operation.PERCENT,
             binding.buttonDivide to Operation.DIVIDE,
             binding.buttonMultiplie to Operation.MULTIPLE,
             binding.buttonPlus to Operation.MULTIPLE,
@@ -59,23 +54,19 @@ class MainActivity : AppCompatActivity() {
             binding.buttonEqual to Operation.EQUAL,
             binding.buttonRemove to Operation.REMOVE
         )
-        // не уверен< что я правильно пользуюсь этими штуками
-        val outputColorLiveData: MutableLiveData<Status> = MutableLiveData()
-        outputColorLiveData.observe(this, Observer {
+
+
+        calculator.outputColor.observe(this) {
             changeColorBasedOnStatus(it, normalColor, errorColor)
-        })
-        val outputTextLiveData: MutableLiveData<String> = MutableLiveData()
-        outputTextLiveData.observe(this, Observer {
+        }
+        calculator.outputText.observe(this) {
             binding.outputText.text = it.toString()
-        })
+        }
 
         actionButtons.forEach { (key, value) ->
             key.setOnClickListener {
                 calculator.makeAction(value)
                 it.startAnimation(animationAlpha)
-                outputColorLiveData.value = calculator.status
-                outputTextLiveData.value = calculator.getOutput()
-                changeColorBasedOnStatus(calculator.status, normalColor, errorColor)
             }
         }
     }
